cc14ad0534adf2e6b8472b500b5166d1055e7cee
Dinesh Kumar <Kumardinesh1908@gmail.com>
2023-10-09 07:15:47 +0530

    added the comments to understand the logic

--- script.js
+++ script.js
@@ -2,36 +2,41 @@
 var buttons = document.getElementsByClassName("button");
 var display = document.getElementById("display");
 
-// display.textContent ;
-var operand1 = "0";
-var operand2 = null;
-var operator = null;
+// Initialize variables for the calculator
+var operand1 = "0"; //first input
+var operand2 = null;  //second input
+var operator = null;  // operator to perform operations
 
+// Function to check if a value is an operator
 function isOperator(value) {
     return value == "+" || value == "-" || value == "*" || value == "/";
 }
 
+// Loop through all the buttons and add click event listeners
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function () {
 
+        // Get the value attribute of the clicked button
         var value = this.getAttribute('data-value');
 
+        // Check the value and perform appropriate actions
         if (isOperator(value)) {
+            // If an operator is clicked
             if (operator !== null) {
-                return;
+                return; // Return if an operator is already set
             }
             operator = value;
             operand1 = parseFloat(display.textContent);
-            console.log(operand1)
-            console.log(operator)
             display.textContent = "0";
         } else if (value == "ac") {
+            // Clear all values and reset the display
             operand1 = "0";
             operand2 = null;
             operator = null;
             display.textContent = "0";
         } else if (value == "sign") {
-            if (operator === null) {
+            // Toggle the sign of the current operand
+            if (operator === null) {   // Check if the operator is null, indicating currently working with the first operand
                 operand1 = -parseFloat(display.textContent);
                 display.textContent = operand1;
             }
@@ -40,6 +45,7 @@ for (var i = 0; i < buttons.length; i++) {
                 display.textContent = operand2;
             }
         } else if (value == ".") {
+            // Add a decimal point to the display
             if (display.textContent.length == 0) {
                 display.textContent = "0" + ".";
             }
@@ -47,7 +53,8 @@ for (var i = 0; i < buttons.length; i++) {
                 display.textContent = display.textContent + '.';
             }
         } else if (value == "%") {
-            if (operator === null) {
+            // Calculate percentage of the current operand
+            if (operator === null) {  // Check if the operator is null, indicating currently working with the first operand
                 operand1 = operand1 / 100;
                 display.textContent = operand1;
             }
@@ -56,12 +63,9 @@ for (var i = 0; i < buttons.length; i++) {
                 operand2 = operand2 / 100;
                 display.textContent = operand2;
             }
-        } 
-        else if (value == "=") {
+        } else if (value == "=") {
+            // Perform calculation when "=" is clicked
             operand2 = parseFloat(display.textContent);
-            console.log(operand1)
-            console.log(operator)
-            console.log(operand2)
 
             var result = calculate(operand1, operator, operand2);
             display.textContent = result;
@@ -69,6 +73,7 @@ for (var i = 0; i < buttons.length; i++) {
             operand2 = null;
             operator = null;
         } else {
+            // Append the clicked value to the display
             if (display.textContent == "0") {
                 display.textContent = value;
             }
@@ -79,6 +84,8 @@ for (var i = 0; i < buttons.length; i++) {
     });
 }
 
+
+// Function to perform calculations based on operator
 function calculate(a, operator, b) {
     switch (operator) {
         case "+": return a + b;
