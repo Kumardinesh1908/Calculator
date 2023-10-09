044379e3bcd4230da0ac2cb3d4d60da2d2f12a47
Dinesh Kumar <Kumardinesh1908@gmail.com>
2023-10-09 06:56:29 +0530

    updated the whole logic and increased the width of calculator

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 var buttons = document.getElementsByClassName("button");
 var display = document.getElementById("display");
 
-display.textContent = 0;
-var operand1 = 0;
+// display.textContent ;
+var operand1 = "0";
 var operand2 = null;
 var operator = null;
-var result = null;
 
 function isOperator(value) {
     return value == "+" || value == "-" || value == "*" || value == "/";
@@ -18,22 +17,26 @@ for (var i = 0; i < buttons.length; i++) {
         var value = this.getAttribute('data-value');
 
         if (isOperator(value)) {
-            if (display.textContent != 0) {
-                operator = value;
-                operand1 = parseFloat(display.textContent);
-                display.textContent = "";
+            if (operator !== null) {
+                return;
             }
+            operator = value;
+            operand1 = parseFloat(display.textContent);
+            console.log(operand1)
+            console.log(operator)
+            display.textContent = "0";
         } else if (value == "ac") {
+            operand1 = "0";
+            operand2 = null;
+            operator = null;
             display.textContent = "0";
         } else if (value == "sign") {
-            if (operand1 == 0) {
-                operand1 = parseFloat(display.textContent);
-                operand1 = -1 * operand1;
+            if (operator === null) {
+                operand1 = -parseFloat(display.textContent);
                 display.textContent = operand1;
             }
             else {
-                operand2 = parseFloat(display.textContent);
-                operand2 = -1 * operand2;
+                operand2 = -parseFloat(display.textContent);
                 display.textContent = operand2;
             }
         } else if (value == ".") {
@@ -44,8 +47,7 @@ for (var i = 0; i < buttons.length; i++) {
                 display.textContent = display.textContent + '.';
             }
         } else if (value == "%") {
-            if (operand1 == 0) {
-                operand1 = parseFloat(display.textContent);
+            if (operator === null) {
                 operand1 = operand1 / 100;
                 display.textContent = operand1;
             }
@@ -54,20 +56,18 @@ for (var i = 0; i < buttons.length; i++) {
                 operand2 = operand2 / 100;
                 display.textContent = operand2;
             }
-        } else if (value == "=") {
+        } 
+        else if (value == "=") {
             operand2 = parseFloat(display.textContent);
-            if (operand2) {
-                result = eval((operand1) + (operator) + (operand2));
-            }
-            else {
-                result = operand1;
-            }
-            if (result) {
-                display.textContent = result;
-                operand1 = result;
-                operand2 = null;
-                operator = null;
-            }
+            console.log(operand1)
+            console.log(operator)
+            console.log(operand2)
+
+            var result = calculate(operand1, operator, operand2);
+            display.textContent = result;
+            operand1 = result;
+            operand2 = null;
+            operator = null;
         } else {
             if (display.textContent == "0") {
                 display.textContent = value;
@@ -79,4 +79,13 @@ for (var i = 0; i < buttons.length; i++) {
     });
 }
 
+function calculate(a, operator, b) {
+    switch (operator) {
+        case "+": return a + b;
+        case "-": return a - b;
+        case "*": return a * b;
+        case "/": return b !== 0 ? a / b : "Can't divide by 0";
+        default: return b;
+    }
+}
 
